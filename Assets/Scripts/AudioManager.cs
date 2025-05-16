a1c7eeaf4c48d5ccbe8294a1c00747f29fb66317
AlisonLoke <alisonloke2@gmail.com>
2025-05-16 11:31:39 +0100

    Added temp music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -14,6 +14,7 @@ public class AudioManager : MonoBehaviour
         if(instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
