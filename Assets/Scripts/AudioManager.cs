04645cdd2dd8ed97554ab01b8ad78f2b6d2038c6
AlisonLoke <alisonloke2@gmail.com>
2025-07-02 14:21:53 +0100

    Brought in new Wwise project

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -11,7 +11,7 @@ public class AudioManager : MonoBehaviour
     private Queue<AK.Wwise.Event> musicQueue = new Queue<AK.Wwise.Event>();
     private bool IsMusicPlaying = false;
     private GameObject currentAudioObject;
-
+    //public AK.Wwise.Event musicEvent;
 
 
     private void Awake()
@@ -27,6 +27,11 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    //private void Update()
+    //{
+    //    Debug.Log("MUSIC IS PLAYING RIGHT NOW");
+    //    musicEvent.Post(gameObject);
+    //}
     //public void QueueMusic(AK.Wwise.Event instrument)
     //{
     //    if (instrument == null) return;
--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,27 +13,72 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BANJO01_IND = 1106709462U;
-        static const AkUniqueID BANJO02_IND = 3686043613U;
-        static const AkUniqueID BANJO03_IND = 2616549120U;
-        static const AkUniqueID BANJO_01 = 1654636977U;
-        static const AkUniqueID BANJO_02 = 1654636978U;
-        static const AkUniqueID BASS01_IND = 2828584415U;
-        static const AkUniqueID BASS02_IND = 3835499564U;
-        static const AkUniqueID BASS03_IND = 1444490905U;
-        static const AkUniqueID BASS_01 = 2197579062U;
-        static const AkUniqueID BASS_02 = 2197579061U;
-        static const AkUniqueID DRUM_SNARE = 4059233419U;
-        static const AkUniqueID DUM_HIHAT = 4075853312U;
-        static const AkUniqueID LEVEL01TRACK = 858850169U;
-        static const AkUniqueID PLAY_DRUM = 2376459254U;
+        static const AkUniqueID LVL2_1_HIHATPLAY = 1029901154U;
+        static const AkUniqueID LVL2_1_HIHATSTOP = 1631337148U;
+        static const AkUniqueID LVL2_1_KICKPLAY = 4260762352U;
+        static const AkUniqueID LVL2_1_KICKSTOP = 2567667346U;
+        static const AkUniqueID LVL2_1_SNAREPLAY = 233758521U;
+        static const AkUniqueID LVL2_1_SNARESTOP = 2824720255U;
+        static const AkUniqueID LVL2_2_BASS = 52528120U;
+        static const AkUniqueID LVL2_2_BELLS = 544256877U;
+        static const AkUniqueID LVL2_2_CHORDS = 3227398492U;
+        static const AkUniqueID LVL2_3_PLUCKS = 3418998824U;
+        static const AkUniqueID LVL2_3_TUNE = 3891698248U;
+        static const AkUniqueID LVL2_3_WOW = 497870267U;
+        static const AkUniqueID NEW_EVENT = 3050945240U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSICLVL2
+        {
+            static const AkUniqueID GROUP = 2762093882U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LVL2_1 = 3868115957U;
+                static const AkUniqueID LVL2_1_HIHATPLAY = 1029901154U;
+                static const AkUniqueID LVL2_1_HIHATSTOP = 1631337148U;
+                static const AkUniqueID LVL2_1_KICKPLAY = 4260762352U;
+                static const AkUniqueID LVL2_1_KICKSTOP = 2567667346U;
+                static const AkUniqueID LVL2_1_SNAREPLAY = 233758521U;
+                static const AkUniqueID LVL2_1_SNARESTOP = 2824720255U;
+                static const AkUniqueID LVL2_2 = 3868115958U;
+                static const AkUniqueID LVL2_3 = 3868115959U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MUSICLVL2
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace KICK
+        {
+            static const AkUniqueID GROUP = 2181839183U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace KICK
+
+    } // namespace SWITCHES
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID LVL2_1_HIHATPLAY = 1029901154U;
+        static const AkUniqueID LVL2_1_HIHATSTOP = 1631337148U;
+        static const AkUniqueID LVL2_1_KICKPLAY = 4260762352U;
+        static const AkUniqueID LVL2_1_KICKSTOP = 2567667346U;
+        static const AkUniqueID LVL2_1_SNAREPLAY = 233758521U;
+        static const AkUniqueID LVL2_1_SNARESTOP = 2824720255U;
+    } // namespace TRIGGERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID LEVEL0_SOUNDBANK = 1170040669U;
-        static const AkUniqueID PLAYDRUMTEST = 1932394983U;
+        static const AkUniqueID LVL2_MUSICBANK = 3224214015U;
     } // namespace BANKS
 
     namespace BUSSES
