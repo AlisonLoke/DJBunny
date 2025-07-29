a4443ddc7314c2d59325c894a04f529d33d2ed6c
AlisonLoke <alisonloke2@gmail.com>
2025-07-29 16:53:55 +0100

    Added dialogue to bus intro cutscene

--- Assets/ScrollingBackGround.cs
+++ Assets/ScrollingBackGround.cs
@@ -5,7 +5,7 @@ public class ScrollingBackGround : MonoBehaviour
 {
     [SerializeField] private RawImage bgImage;
     [SerializeField] private float x,y;
-    // Update is called once per frame
+  
     void Update()
     {
         bgImage.uvRect = new Rect(bgImage.uvRect.position + new Vector2(x, y) * Time.deltaTime,bgImage.uvRect.size);
